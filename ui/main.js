965ef3ae35874b8ac2468c6be1a12a49a1cd36f3
Naveen S <30658932+NaveenEzio@users.noreply.github.com>
2017-08-13 17:55:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,6 @@ console.log('Loaded!');
 var img = document.getElementById('1img');
 
 img.onclick =function(){
-    img.style.marginRight = "100px" ;
+    //img.style.marginRight = "100px" ;
+    var interval = setInterval(moveRight,50);
 }
