aae5132c58d0426d81bad06c31883eb7844a4fc1
Naveen S <30658932+NaveenEzio@users.noreply.github.com>
2017-08-13 17:44:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
 console.log('Loaded!');
 
 
-var img = document.GetElementById('img1')
+var img = document.getElementById('1img') ;
+
+ img.onclick =  fuction() {
+    
+    img.style.marginLeft = '100px';
+    
+};
